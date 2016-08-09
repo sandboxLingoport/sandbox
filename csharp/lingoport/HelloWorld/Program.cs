a4814b74c86fa7c30948f3e73a924c720cd70531
Michael Asnes <masnes@lingoport.com>
2016-08-09 15:48:04 -0600

    Added new csharp helloworld example

---
+++ csharp/lingoport/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String greeting = "Hello";
+            greeting += " World!";
+
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine(greeting);
+            Console.WriteLine(now.ToLongDateString());
+        }
+    }
+}
