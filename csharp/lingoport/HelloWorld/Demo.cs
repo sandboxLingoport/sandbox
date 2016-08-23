a74e6b5e03e25fc1f0b36f2bd568b7f2c4efaec6
Michael Asnes <masnes@lingoport.com>
2016-08-23 11:05:18 -0600

    added csharp code with many issues

---
+++ csharp/lingoport/HelloWorld/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Demo
+    {
+        // Show off various issues detected by Globalyzer
+        Encoding asci = Encoding.ASCII;
+        Encoding latin = Encoding.GetEncoding("ISO-8859-1");
+        CharSet charset = CharSet.Auto;
+        CaseInsensitiveComparer comparer = new CaseInsensitiveComparer();
+        String lowercase = "U.S Capitals".ToLower();
+
+        public void doSomething()
+        {
+            asci.GetBytes("Hello");
+            lowercase.ToUpper();
+        }
+    }
+}
