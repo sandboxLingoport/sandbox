e9b85ab0ab593feb44e6aafde86e7be52f6ca35a
Michael Asnes <masnes@lingoport.com>
2016-08-23 17:42:01 -0600

    Added more issues to javascript
    synced HelloWorld.html and HelloWorld.js

--- javascript/lingoport/HelloWorld.js
+++ javascript/lingoport/HelloWorld.js
@@ -1,16 +1,14 @@
-<head/>
-<body>
-  <script>
-    var greeting = "Hello";
-    greeting += " World!";
+var greeting = "Hello";
+greeting += " World!";
 
-    var today = new Date();
-    var dateString = "{0}/{1}/{2}".format(today.getMonth(), today.getDay(), today.getYear);
+var today = new Date();
+var dateString = today.getMonth() + "/" + today.getDay() + "/" + today.getYear();
 
-    alert(greeting);
-    alert("Today is {0}".format(dateString));
+document.write("<p>" + greeting + "</p>");
+document.write("<p>" + "Displaying date in M/dd/yy format:" + "</p>");
+document.write("<p>" + "Today is " + dateString + "</p>");
+document.write("<p>" + "Parsing '2.570', it's a very small number in the U.S, but very large in Europe!")
+document.write(" Result: " + parseFloat("2.570") + "</p>")
 
-    var logMessage = "Logging Message - Don't translate me";
-    console.log(logMessage);
-  </script>
-</body>
+var logMessage = "Logging Message - Don't translate me";
+console.log(logMessage);
