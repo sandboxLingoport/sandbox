0edaa536d8a8b53f3d027ba79e19f3f84e2acd83
masnesLingoport <masnes@lingoport.com>
2017-03-09 15:59:57 -0700

    Add another string to test updates

--- java/lingoport/com/lingoport/HelloWorld.java
+++ java/lingoport/com/lingoport/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		String greeting = "Hello";
 		String greeting2 = "not hello";
+		String string3 = "Another string";
 		greeting += " World";
 		String goodbye = " Goodbye World";
 
