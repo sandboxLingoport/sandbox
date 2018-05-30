4da5cd659e4e86e49b83b36673648d6a8cb5ac8d
Mike MacDougall <muo@qlik.com>
2018-05-30 10:35:38 -0400

    Changes for testing

--- javascript/lingoport/public/js/test.js
+++ javascript/lingoport/public/js/test.js
@@ -1,29 +1,40 @@
 function dateMethod() {
-    let dateField = document.querySelector('#dateResult')
+  let dateField = document.querySelector('#dateResult')
 
-    let date = new Date()
-    let timeString = date.toLocaleString();
-    dateField.innerHTML += `Current time is ${timeString}`
+  let date = new Date()
+  let timeString = date.toLocaleString();
+  dateField.innerHTML = `Current time is ${timeString}`
 
-  }
+}
 
-  function submitName() {
-    let t = document.getElementById('userName').value
-    let splitValue = t.split(' ')
-    document.getElementById("demo").innerHTML = splitValue[1]
-  }
+function submitName() {
+  let t = document.getElementById('userName').value
+  let splitValue = t.split(' ')
+  document.getElementById("demo").innerHTML = splitValue[1]
+}
 
+function sortString() {
+  var items = ['Òphelia', 'Frànklin', 'Frìàr', 'frìâr', 'fìlâr']
+  return items.sort()
+}
 
-  function getRandomCharacter() {
-    let randomCharacter = randomNumber()
-    console.log(randomCharacter)
-    let characterCode = String.fromCharCode(randomCharacter)
-    document.getElementById('randomCharacter').innerHTML = characterCode
-  }
+function getRandomCharacter() {
+  let randomCharacter = randomNumber()
+  console.log(randomCharacter)
+  let characterCode = String.fromCharCode(randomCharacter)
+  document.getElementById('randomCharacter').innerHTML = characterCode
+}
 
-  // Create a random character
-  function randomNumber() {
-    let randomness = Math.floor((Math.random() * 65535) + 1);
-    console.log(randomness)
-    return randomness
-  }
+// Create a random character
+function randomNumber() {
+  let randomness = Math.floor((Math.random() * 65535) + 1);
+  console.log(randomness)
+  return randomness
+}
+
+//This is just a comment with a plus + to check for contenation rules.
+function returnDate() {
+  var myDate = new Date.UTC(2019, 5, 9, 5, 40, 4)
+  var theDate = new Intl.DateTimeFormat(myDate)
+  return theDate
+}
